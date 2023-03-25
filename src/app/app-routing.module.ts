d7e8c65617bbb419a7d2f89c01f70eefd59eed47
githubsnjv <snjvacoz@gmail.com>
2023-03-25 18:45:16 +0530

    report component creation only

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { MachinesComponent } from './components/machines/machines.component';
+import { ReportReportComponent } from './components/report-report/report-report.component';
 
 const routes: Routes = [
   {path:'entry',component:FormsComponent},
   {path:'mach/:id',component:MachinesComponent},
   {path:'dash',component:DashboardComponent},
-  {path:"",redirectTo:'dash'}
+  {path:"",redirectTo:'dash'},
+  {path:'report',component:ReportReportComponent}
 ];
 
 @NgModule({
