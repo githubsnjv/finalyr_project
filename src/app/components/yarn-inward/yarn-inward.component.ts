8c04c15131c88f0bc68489b8b77283aa16f638cb
Gowtham C <gowthamchandrasekaran01@gamil.com>
2023-04-17 16:20:07 +0530

    Form backend linked

--- src/app/components/yarn-inward/yarn-inward.component.ts
+++ src/app/components/yarn-inward/yarn-inward.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-yarn-inward',
@@ -6,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./yarn-inward.component.css']
 })
 export class YarnInwardComponent implements OnInit {
+  yarnInward = new FormGroup({
+    yarnname : new FormControl(''),
+    yarncount : new FormControl(''),
+    no_of_bags  : new FormControl(''),
+    no_of_halfbags : new FormControl(''),
+    bags_in_kgs : new FormControl(''),
+    received_date : new FormControl(''),
+    vehicle_no : new FormControl(''),
+    mill : new FormControl(''),
+    company : new FormControl(''),
+
+  });
+
+  onSubmit(){
+    console.log(this.yarnInward.value);
+  }
+
 
   constructor() { }
 
